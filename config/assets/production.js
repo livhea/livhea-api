238c76d187fdd81a38352c24a1874dd76b8a11dd
Vipul <vbhopal@gmail.com>
2016-07-19 00:49:36 +0530

    Production Assets

--- config/assets/production.js
+++ config/assets/production.js
@@ -9,15 +9,18 @@ module.exports = {
       js: [
         'public/lib/jquery/dist/jquery.min.js',
         'public/lib/semantic/dist/semantic.min.js',
-        'public/lib/angular/angular.min.js',
-        'public/lib/angular-resource/angular-resource.min.js',
-        'public/lib/angular-animate/angular-animate.min.js',
-        'public/lib/angular-messages/angular-messages.min.js',
-        'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-        'public/lib/angular-ui-utils/ui-utils.min.js',
-        'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-        'public/lib/angular-file-upload/angular-file-upload.min.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
+        'public/lib/angular/angular.js',
+        'public/lib/angular-resource/angular-resource.js',
+        'public/lib/angular-animate/angular-animate.js',
+        'public/lib/angular-messages/angular-messages.js',
+        'public/lib/angular-ui-router/release/angular-ui-router.js',
+        'public/lib/angular-ui-utils/ui-utils.js',
+        'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+        'public/lib/angular-sanitize/angular-sanitize.js',
+        'public/lib/angular-file-upload/angular-file-upload.js',
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
+        'public/lib/moment/min/moment-with-locales.min.js',
+        'public/lib/fullcalendar/dist/fullcalendar.js'
       ]
     },
     css: 'public/dist/application.min.css',
